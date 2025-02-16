0a4c7f69f860eb7c9efc410ccadac0c8d2ccda90
Shriganesh Bhide <shriganesh2019@iiitkottayam.ac.in>
2025-02-17 01:47:06 +0530

    Fix: changed for avoiding build errors

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,12 +1,11 @@
 // client.ts (your existing auth client)
-import { PUBLIC_BETTER_AUTH_URL } from "$env/static/public";
+import { dev } from "$app/environment";
 import { createAuthClient } from "better-auth/client";
 import { writable } from "svelte/store";
 
 export const authClient = createAuthClient(
   {
-    baseURL: PUBLIC_BETTER_AUTH_URL
-    // baseURL: dev ? 'http://localhost:5173': 'https://flex-ruddy.vercel.app'// the base url of your auth server
+    baseURL: dev ? 'http://localhost:5173': 'https://flex-ruddy.vercel.app'// the base url of your auth server
   }
 );
 // console.log(process.env.BETTER_AUTH_URL as string, " is the ")
