235a6309fc17dc268152701e9e9a7e20a478a887
Shriganesh Bhide <shriganesh2019@iiitkottayam.ac.in>
2025-02-17 01:42:00 +0530

    public instead of private environment variable.

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,11 +1,11 @@
 // client.ts (your existing auth client)
-import { BETTER_AUTH_URL } from "$env/static/private";
+import { PUBLIC_BETTER_AUTH_URL } from "$env/static/public";
 import { createAuthClient } from "better-auth/client";
 import { writable } from "svelte/store";
 
 export const authClient = createAuthClient(
   {
-    baseURL: BETTER_AUTH_URL
+    baseURL: PUBLIC_BETTER_AUTH_URL
     // baseURL: dev ? 'http://localhost:5173': 'https://flex-ruddy.vercel.app'// the base url of your auth server
   }
 );
