2f98520971fbf647b5939997a66c7f996d180da6
Shriganesh Bhide <shriganesh2019@iiitkottayam.ac.in>
2025-02-20 23:09:23 +0530

    redirect url change test.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,8 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./server/db"; // your drizzle instance
 import * as schema from '../lib/server/db/schema';
+import { env } from "$env/dynamic/private";
+
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
@@ -16,6 +18,6 @@ export const auth = betterAuth({
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       },
     },
-    trustedOrigins: ["https://super-duper-system-rvqqp5j4qppcgq4-5173.app.github.dev"],
+    trustedOrigins: env.ALLOWED_ORIGINS?.split(",") || [],
   });
   
