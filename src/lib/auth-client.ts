b084bb63481b1dd54605e44ee414fd81d5d56ce7
Shriganesh Bhide <shriganesh2019@iiitkottayam.ac.in>
2025-03-30 02:01:29 +0530

    Feat: Added header and footer and avatar coming soon.

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,19 +1,26 @@
 // client.ts (your existing auth client)
 // import { dev } from "$app/environment";
+import type { Session } from "better-auth";
 import { createAuthClient } from "better-auth/client";
-import { writable } from "svelte/store";
-
+import { writable, type Writable } from "svelte/store";
 export const authClient = createAuthClient(
   {
-    baseURL: 'https://flex-ruddy.vercel.app'// the base url of your auth server
+    baseURL: 'http://localhost:5173/'// the base url of your auth server
   }
 );
 // console.log(process.env.BETTER_AUTH_URL as string, " is the ")
-export const session = writable(null);
+export const session: Writable<Session | null> = writable(null);
 
+// Fetch and update session on startup
+const fetchSession = async () => {
+  const userSession = await authClient.getSession();
+  if(userSession.data != undefined && userSession.data.session)
+  session.set(userSession.data.session);
+};
+fetchSession(); // Ensure session is populated on app load
 // Sign-in function
 export const signIn = async () => {
   const data = writable(await authClient.signIn.social({ provider: "google" }));
   // console.log(data);
-  return data;
+  return {data};
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,43 @@
-<script>
+<script lang="ts">
 	import { goto } from '$app/navigation';
 	import CodeforcesProfile from '$lib/components/codeforcesProfiles/profileBuilder.svelte';
-
-	let userInput = $state('Shriganesh');
-	function GoHome() {
-		goto('/Home');
-	}
+	import type { PageData } from './$types';
+	import Button from '$lib/components/ui/button/button.svelte';
+	let { data }: { data: PageData } = $props();
+	let userId = $state(data.session?.user.id);
 </script>
 
-<CodeforcesProfile />
+<!-- {data.session?.session.id} -->
+<div class="Page">
+	<div class="Profiles">
+		<CodeforcesProfile />
+	</div>
+
+	<footer>
+		{#if data.session?.user.id != undefined}
+			<Button
+				variant="link"
+				class="animate-text-gradient relative mt-32 inline-flex items-center justify-center bg-gradient-to-r from-[#ACACAC] via-[#363636] to-[#ACACAC] bg-[200%_auto] bg-clip-text px-6 py-3 text-center text-3xl font-medium text-transparent transition-all after:absolute after:bottom-0 after:left-0 after:h-[2px] after:w-0 after:bg-gradient-to-r after:from-[#ACACAC] after:via-[#363636] after:to-[#ACACAC] after:transition-all after:duration-300 after:content-[''] hover:scale-105 hover:after:w-full"
+				href="/{userId}"
+			>
+				View your profile card
+			</Button>
+		{/if}
+	</footer>
+</div>
+
+<style>
+	.Profiles {
+		/* padding: 20px; */
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+	footer {
+		justify-self: center;
+	}
+	.Page {
+		height: 100%;
+	}
+</style>
